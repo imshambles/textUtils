5fd5e4ba83203ad3b85b8207caeb276c60d859aa
imshambles <vishwas.latiyan007@gmail.com>
2023-01-04 16:36:27 +0530

    added hashrouter

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import TextForm from "./components/TextForm";
 import About from "./components/About";
 import React, { useState } from "react";
 import Alert from "./components/Alert";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light"); // Whether dark mode is enabled or not
@@ -34,6 +34,7 @@ function App() {
   return (
     <>
       <Router>
+        <HashRouter basename="/">
         <Navbar
           title="TextUtils"
           mode={mode}
@@ -56,6 +57,7 @@ function App() {
             </Route>
           </Routes>
         </div>
+        </HashRouter>
       </Router>
     </>
   );
